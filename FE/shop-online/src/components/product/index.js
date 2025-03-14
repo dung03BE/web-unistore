89a566348e09c0ebc930113ad96a61077d83a681
dung03BE <dung2003hn@gmail.com>
2025-03-14 09:30:29 +0700

    update home get sp by category

--- FE/shop-online/src/components/product/index.js
+++ FE/shop-online/src/components/product/index.js
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
-import { getAdList, getProductList } from "../../services/productService";
+import { getAdList, getProductList, getProductListByCategoryId } from "../../services/productService";
 import React from 'react';
 import { Pagination } from 'antd';
 import "./product.scss";
 import ProductItem from "./ProductItem";
 import ProductCarousel from "./ProductCarousel";
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
-function Product({ navigate }) {
+function Product() {
     const [product, setProduct] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [adImages, setAdImages] = useState([]);
     const [totalItems, setTotalItems] = useState(0);
     const pageSize = 8;
     const [loading, setLoading] = useState(false);
-    const [searchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
     const search = searchParams.get('search') || '';
-    const [minPriceFilter, setMinPriceFilter] = useState(''); // State cho min price
+    const [minPriceFilter, setMinPriceFilter] = useState('');
     const [maxPriceFilter, setMaxPriceFilter] = useState('');
     const minPriceParam = searchParams.get('minPrice');
     const maxPriceParam = searchParams.get('maxPrice');
+    const categoryIdParam = searchParams.get('categoryId');
 
-    // Chuyển đổi minPrice và maxPrice sang số nếu có
     const minPrice = minPriceParam ? parseFloat(minPriceParam) : undefined;
     const maxPrice = maxPriceParam ? parseFloat(maxPriceParam) : undefined;
-
+    const categoryId = categoryIdParam ? parseInt(categoryIdParam) : undefined;
+    const [selectedCategory, setSelectedCategory] = useState(categoryId);
     useEffect(() => {
         const fetchAds = async () => {
             const ads = await getAdList();
@@ -36,20 +38,27 @@ function Product({ navigate }) {
     useEffect(() => {
         const fetchApi = async () => {
             setLoading(true);
+            let result;
 
-            // Gọi getProductList với các tham số lọc (nếu có)
-            const result = await getProductList(currentPage - 1, pageSize, minPrice, maxPrice, search);
+            if (categoryId) {
+                // Nếu có categoryId, gọi API lọc theo category
+                result = await getProductListByCategoryId(currentPage - 1, pageSize, categoryId);
+            } else {
+                // Nếu không có categoryId, gọi API lọc theo các tham số khác
+                result = await getProductList(currentPage - 1, pageSize, minPrice, maxPrice, search);
+            }
 
             setProduct(result.content);
             setTotalItems(result.totalElements);
             setLoading(false);
         };
         fetchApi();
-    }, [currentPage, search, minPrice, maxPrice]); // Thêm minPrice và maxPrice vào dependency array
+    }, [currentPage, search, minPrice, maxPrice, categoryId]);
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
+
     const handlePriceFilterChange = (e) => {
         if (e.key === 'Enter') {
             const newParams = new URLSearchParams(searchParams);
@@ -63,9 +72,18 @@ function Product({ navigate }) {
             } else {
                 newParams.delete('maxPrice');
             }
-            navigate(`/?${newParams.toString()}`); // Chuyển hướng đến URL mới
+            navigate(`/?${newParams.toString()}`);
         }
     };
+
+    const handleCategoryFilter = (categoryId) => {
+        const newParams = new URLSearchParams(searchParams);
+        newParams.set('categoryId', categoryId);
+        setCurrentPage(1); // Reset page to 1
+        navigate(`/?${newParams.toString()}`);
+        setSelectedCategory(categoryId);
+    };
+
     return (
         <>
             <ProductCarousel images={adImages} />
@@ -74,15 +92,15 @@ function Product({ navigate }) {
                     <i className="icon-filter"></i>
                     Lọc
                 </button>
-                <button className="filter-option">
+                <button className={`filter-option ${selectedCategory === 1 ? 'selected' : ''}`} onClick={() => handleCategoryFilter(1)}>
                     <i className="icon-filter2"></i>
                     Samsung
                 </button>
-                <button className="filter-option">
+                <button className={`filter-option ${selectedCategory === 2 ? 'selected' : ''}`} onClick={() => handleCategoryFilter(2)}>
                     <i className="icon-filter3"></i>
-                    iPhone
+                    Apple
                 </button>
-                <button className="filter-option">
+                <button className={`filter-option ${selectedCategory === 3 ? 'selected' : ''}`} onClick={() => handleCategoryFilter(3)}>
                     <i className="icon-filter4"></i>
                     Oppo
                 </button>
@@ -92,14 +110,14 @@ function Product({ navigate }) {
                         placeholder="Giá min"
                         value={minPriceFilter}
                         onChange={(e) => setMinPriceFilter(e.target.value)}
-                        onKeyDown={handlePriceFilterChange} // Xử lý sự kiện onKeyDown
+                        onKeyDown={handlePriceFilterChange}
                     />
                     <input
                         type="number"
                         placeholder="Giá max"
                         value={maxPriceFilter}
                         onChange={(e) => setMaxPriceFilter(e.target.value)}
-                        onKeyDown={handlePriceFilterChange} // Xử lý sự kiện onKeyDown
+                        onKeyDown={handlePriceFilterChange}
                     />
                 </div>
             </div>
